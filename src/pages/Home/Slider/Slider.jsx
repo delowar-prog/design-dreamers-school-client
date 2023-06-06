401608773dd3365eede08ea71d4e69797772b3b8
Delowar Hossain <bscdelowar@yahoo.com>
2023-06-07 02:37:09 +0600

    carosal slider setup done

--- src/pages/Home/Slider/Slider.jsx
+++ src/pages/Home/Slider/Slider.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import slider1 from '../../../assets/slider/1.jpg'
+import slider2 from '../../../assets/slider/2.jpg'
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div>
+    <Carousel showArrows={true} autoPlay={true} infiniteLoop={true} interval={10000}>
+                <div>
+                    <img src={slider1} />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src={slider2} />
+                    <p className="legend">Legend 2</p>
+                </div>
+            </Carousel>
+    </div>
   )
 }
 
