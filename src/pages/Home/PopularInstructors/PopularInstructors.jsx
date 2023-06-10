5cc8316317008e0d770a92274e6a2390df96b8ba
Delowar Hossain <bscdelowar@yahoo.com>
2023-06-10 10:20:16 +0600

    popular instructors loaded from db & show home pase done

--- src/pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -4,9 +4,15 @@ import 'swiper/css';
 import "swiper/css/pagination";
 import { Pagination } from 'swiper';
 import Container from '../../../components/Container/Container';
-import slideImg from '../../../assets/classes/fashion.jpg'
+import { Fragment, useEffect, useState } from 'react';
 
 const PopularInstructors = () => {
+  const [instructors, setInstructors] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:5000/instructors/topsix')
+      .then(res => res.json())
+      .then(data => setInstructors(data))
+  }, [])
   return (
     <div className='bg-[#C25582]'>
       <Container>
@@ -14,22 +20,33 @@ const PopularInstructors = () => {
           <SectionTitle heading={'Popular Instructors'} subHeading={'Justify our comments'}></SectionTitle>
           <div>
             <Swiper
-              spaceBetween={50}
-              slidesPerView={3}
+              spaceBetween={10}
+              slidesPerView={4}
               pagination={{
                 clickable: true,
               }}
               modules={[Pagination]}
               className="mySwiper"
+              style={{paddingBottom:'40px'}}
             >
-              <SwiperSlide className='relative cursor-pointer'>
-                <img src={slideImg}></img>
-                  <h1>Graphics Design</h1>
-                  <h3>Students: 52</h3>
-              </SwiperSlide>
-              <SwiperSlide><img src={slideImg}></img></SwiperSlide>
-              <SwiperSlide><img src={slideImg}></img></SwiperSlide>
-              <SwiperSlide><img src={slideImg}></img></SwiperSlide>
+              {
+                instructors.map(instructor => <Fragment key={instructor._id}>
+                  <SwiperSlide className='relative cursor-pointer'>
+                    <div className='instructor-items bg-[#0D0D0D] border-b-4 border-pink-400 text-center text-white space-y-2' >
+                      <div className='relative'>
+                        <img src={instructor.image} className='w-full h-64'></img>
+                        <div className='absolute overlay w-full h-64 top-0'></div>
+                      </div>
+                      <h1 className='text-center font-bold text-xl text-pink-400 my-5 uppercase'>{instructor.name}</h1>
+                      <p>Email: {instructor.email}</p>
+                      <p>Number of Classes : {instructor.taken_class}</p>
+                      <div className='w-full flex justify-center items-center'>
+                        <button className='bg-sky-500 my-5 py-1 px-5 text-white hover:bg-sky-600 uppercase'>See the Classes</button>
+                      </div>
+                    </div>
+                  </SwiperSlide>
+                </Fragment>)
+              }
             </Swiper>
           </div>
         </div>
