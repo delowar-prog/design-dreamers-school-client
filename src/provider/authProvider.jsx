a21af0f81c9f9b40626f8b86dfcf84e5194efedd
Delowar Hossain <bscdelowar@yahoo.com>
2023-06-11 11:52:36 +0600

    send user info to server & receive a token to access valid user & set localstoreage done

--- src/provider/authProvider.jsx
+++ src/provider/authProvider.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react"
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import axios from "axios";
 
 export const AuthContext = createContext(null)
 const AuthProvider = ({children}) => {
@@ -43,7 +44,18 @@ const AuthProvider = ({children}) => {
         const unSubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser)
             //get & set into localstorage
-            setLoading(false)
+            if(currentUser){
+                axios.post(`http://localhost:5000/jwt`,{
+                    email:currentUser.email
+                })
+                .then(result=>{
+                    localStorage.setItem('user_access_key', result.data.token)
+                    setLoading(false)
+                })
+            }else{
+                localStorage.removeItem('user_access_key')
+            }
+            
         })
         return () => {
             return unSubscribe()
